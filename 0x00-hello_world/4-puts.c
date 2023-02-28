b1973850073e6ca1095649453c5a952f0c6ef457
Natiyid <natiyidenekachew79@gmail.com>
2023-02-28 03:22:12 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 
-/** 
- * main - prints "Programming is like building a multilingual 
+/**
+ * main - prints "Programming is like building a multilingua
  * puzzle, followed by a new line.
  * Return: Always 0.
  */
