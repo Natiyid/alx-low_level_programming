165b3a1d24fadc64c1a3406e04460e3367b52515
Natiyid <natiyidenekachew79@gmail.com>
2023-03-02 05:49:32 -0800

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from 
+ * @n: The number to start printing from
  * Return: Always 0.
  */
 
@@ -40,7 +40,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d, ",n);
+	printf("%d, ", n);
 	}
 	}
 	}
